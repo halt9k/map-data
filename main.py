29336a9b4687c3747b04cfa6a0545b18e9fc1bfb
halt <halt9000@protonmail.com>
2022-08-30 17:02:06 +0300

    call chain sketch

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ le_year = 2019
 th_expect_developed = 65.0
 th_expect_modern = 76.0
 
+df_ru_pc = get_population_change_ru_2000_to_2020()
 
 df_pc = get_population_change_2000_to_2020()
 df_le = get_life_expectancy(le_year)
@@ -49,12 +50,14 @@ df_sample_desc += translations.NOBELS + ", 2022;"
 
 bins = np.arange(50, 200 + 1, 5)
 e1, e2 = th_expect_developed, th_expect_modern
-plot_growth_hist(df, bins, df_desc, stop=False, th1=e1, th2=e2)
+# plot_growth_hist(df, bins, df_desc, stop=False, th1=e1, th2=e2)
 
 bins = np.arange(50, 200 + 1, 5)
 e1, e2 = th_expect_developed, th_expect_modern
-plot_growth_hist(df_sample, bins, df_sample_desc, stop=False, th1=e1, th2=e2)
+# plot_growth_hist(df_sample, bins, df_sample_desc, stop=False, th1=e1, th2=e2)
 
 # = 90% ...  140%
-plot_map(df, col_min=80, col_max=150, show_info = False, caption_text=df_desc, wait=False, figN=2)
-plot_map(df_sample, col_min=90, col_max=125, show_info = True, caption_text=df_sample_desc, wait=True, figN=3)
+# plot_map(df, col_min=80, col_max=150, show_info = False, caption_text=df_desc, wait=False)
+# plot_map(df_sample, col_min=90, col_max=125, show_info = True, caption_text=df_sample_desc, wait=True)
+
+plot_ru_map(df_ru_pc, col_min=90, col_max=125, caption_text="RU", wait=True)
