010224b2337ab18440f48a5430e623de8bf0b5ba
halt <halt9000@protonmail.com>
2022-10-22 04:10:17 +0300

    refractor continues

---
+++ consts.py
@@ -0,0 +1,22 @@
+# values here are only for assert purpose:
+# to ensure thet descriptions match data match filters
+
+# main infographic target, how population changed (%) between
+C_FROM_Y = 2000
+C_TO_Y = 2020
+
+# threshold to remove countries with unstable %
+# (total population must be higher than)
+CT_POPULATION_M = 5
+# CT_POPULATION_AT_Y = last year
+
+# threshold to remove countries without education systems
+# (amount of nobel prize winners higher than)
+CT_NOBELS = 2
+CT_NOBELS_AT_Y = 2022
+
+# threshold to remove countries without healthcare systems
+# (life expectancy must be higher than)
+CT_LE_DELEVOPED = 65.0
+CT_LE_MODERN = 76.0
+CT_LE_AT_Y = 2019
